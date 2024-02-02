a7a7f05ffe81e99cc44b0328e540218a6b6c7f1c
Sumit Adep <adepsumit123@gmail.com>
2024-02-02 14:05:10 +0530

    Added a task loop

--- workspace/FreeRTOS/WDT/program1/main/program1.c
+++ workspace/FreeRTOS/WDT/program1/main/program1.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <esp_log.h>
+
+static void Task_Handler(void *arg)
+{
+char *tag="Task-Loop";
+while(1)
+{
+ESP_LOGI(tag,"Running...");
+}
+}
+
 
 void app_main(void)
 {
-while(1)
-{
-printf("Infinite loop\n");
-}
+xTaskCreate(Task_Handler,"Task-loop",2*1024,NULL,5,NULL);
 }
