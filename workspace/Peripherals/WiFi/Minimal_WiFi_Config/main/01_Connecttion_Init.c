5466e3e60c303c79074bfc505c7263b129ba2881
Sumit Adep <adepsumit123@gmail.com>
2024-02-09 17:16:56 +0530

    Renaming...

---
+++ workspace/Peripherals/WiFi/Minimal_WiFi_Config/main/01_Connecttion_Init.c
@@ -0,0 +1,71 @@
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+#define WIFI_SSID      "Samsung"
+#define WIFI_PASS      "66778899"
+#define MAXIMUM_RETRY  5
+
+static EventGroupHandle_t s_wifi_event_group;
+
+#define WIFI_CONNECTED_BIT 1<<0
+#define WIFI_FAIL_BIT      1<<1
+
+static const char *TAG = "wifi station";
+
+static void event_handler(void*,esp_event_base_t,int32_t,void*);
+
+void wifi_init_sta(void);
+
+void app_main(void)
+{
+	nvs_flash_init();
+	wifi_init_sta();
+}
+
+void wifi_init_sta(void)
+{
+	s_wifi_event_group = xEventGroupCreate();
+    esp_netif_init();
+    esp_event_loop_create_default();
+    esp_netif_create_default_wifi_sta();
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    esp_wifi_init(&cfg);
+    esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, NULL);
+    wifi_config_t wifi_config = {.sta = {.ssid = WIFI_SSID, .password = WIFI_PASS}};
+    esp_wifi_set_mode(WIFI_MODE_STA);
+    esp_wifi_set_config(WIFI_IF_STA, &wifi_config);
+    esp_wifi_start();
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+ 	EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+            pdFALSE,
+            pdFALSE,
+            portMAX_DELAY);  
+  	 if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
+                 WIFI_SSID, WIFI_PASS);
+    } else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
+                 WIFI_SSID, WIFI_PASS);
+    } else {
+        ESP_LOGE(TAG, "UNEXPECTED EVENT");
+    }   
+}
+
+void event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
+        esp_wifi_connect();
+    
+}
